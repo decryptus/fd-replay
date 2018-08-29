28b6ec5b23f2fd70dd73ac3fbbcb0c2c20c13fd8
Adrien Delle Cave <adrien.delle.cave@commandersact.com>
2018-08-29 21:50:07 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# fd-replay project
+
+## Installation
+
+pip install fd-replay
+
+### Running daemon
+
+`fd-replay -c <conffile> -p <pidfile> --logfile <logfile>`
+
+### Running foreground
+
+`fd-replay -f -c <conffile> -p <pidfile> --logfile <logfile>`
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import os
+from setuptools import find_packages, setup
+
+version                = '0.0.3'
+
+current_dir            = os.path.abspath(os.path.dirname(__file__))
+requirements           = [line.strip() for line in open(os.path.join(current_dir, 'requirements.txt'), 'r').readlines()]
+version_file           = os.path.join(current_dir, 'VERSION')
+readme_file            = os.path.join(current_dir, 'README.md')
+long_desc              = None
+long_desc_content_type = None
+
+if os.path.isfile(version_file):
+    version = open(version_file, 'r').readline().strip() or version
+
+if os.path.isfile(readme_file):
+    long_desc = open(readme_file, 'r').read()
+    long_desc_content_type = 'text/markdown'
+
+setup(
+    name                          = 'fd-replay',
+    version                       = version,
+    description                   = 'fd-replay',
+    author                        = 'Adrien Delle Cave',
+    author_email                  = 'pypi@doowan.net',
+    license                       = 'License GPL-2',
+    url                           = 'https://github.com/decryptus/fd-replay',
+    scripts                       = ['bin/fd-replay'],
+    packages                      = find_packages(),
+    install_requires              = requirements,
+    long_description              = long_desc,
+    long_description_content_type = long_desc_content_type
+)
